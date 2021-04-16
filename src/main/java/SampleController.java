4c4dc30c3660405cc50e651a8707e2aac84946d1
Camilo Ibañez <camilo.ibanez@blackboard.com>
2021-04-15 20:26:55 -0500

    test

--- src/main/java/SampleController.java
+++ src/main/java/SampleController.java
@@ -11,7 +11,7 @@ public class SampleController {
     @RequestMapping("/")
     @ResponseBody
     String home() {
-        return "Hello World2!";
+        return "Hello World3!";
     }
 
     public static void main(String[] args) throws Exception {
