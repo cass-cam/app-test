c973a83a3914260b7afcf726579e5bd4de071019
Camilo Ibañez <camilo.ibanez@blackboard.com>
2021-04-16 21:00:42 -0500

    test

--- execute3.sh
+++ execute3.sh
@@ -1,4 +1,3 @@
 sgeks1=`aws ec2 describe-security-groups --region us-east-1 | grep eks-cluster-sg-test-app -A 20 | grep GroupId | sed 's/^[ \t]*"GroupId": "//' | sed 's/",//' |  sed '$d'`
-sgeks2=`aws ec2 describe-security-groups --region us-east-1 | grep test-app-eks_cluster_sg -A 25 | grep GroupId | sed 's/^[ \t]*"GroupId": "//' | sed 's/",//'`
 sgasg=`aws ec2 describe-security-groups --region us-east-1 | grep SG_app -A 20 | grep GroupId | sed 's/^[ \t]*"GroupId": "//' | sed 's/",//'`
 aws ec2 authorize-security-group-ingress --group-id $sgeks1 --protocol all --port -1 --source-group $sgasg
