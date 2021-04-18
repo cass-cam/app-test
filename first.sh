95e30a4df02ba58595d575c8ebc12327b839844c
Camilo Ibañez <camilo.ibanez@blackboard.com>
2021-04-17 20:42:09 -0500

    deploy eks

---
+++ first.sh
@@ -0,0 +1,7 @@
+cp Jenkinsfile-original Jenkinsfile
+cd kubernetes
+cp eks.tf-original eks.tf
+cd ../jenkins-ec2/
+cp alb.tf-original alb.tf
+cp ec2-ASG.tf-original ec2-ASG.tf
+cp sg.tf-original sg.tf
