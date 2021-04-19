9a3b7d93cffe76e8c144689327dfb1583d84edf9
Camilo Ibañez <camilo.ibanez@blackboard.com>
2021-04-19 12:09:40 -0500

    fix sg eks

--- execute3.sh
+++ execute3.sh
@@ -1,3 +1,3 @@
-sgeks1=`aws ec2 describe-security-groups --region us-east-1 | grep eks-cluster-sg-test-app -A 20 | grep GroupId | sed 's/^[ \t]*"GroupId": "//' | sed 's/",//' |  sed '$d'`
+sgeks1=`aws ec2 describe-security-groups --region us-east-1 | grep eks-cluster-sg-test-app -A 20 | grep GroupId | sed 's/^[ \t]*"GroupId": "//' | sed 's/",//' |  sed '2,$d'`
 sgasg=`aws ec2 describe-security-groups --region us-east-1 | grep SG_app -A 20 | grep GroupId | sed 's/^[ \t]*"GroupId": "//' | sed 's/",//'`
 aws ec2 authorize-security-group-ingress --group-id $sgeks1 --protocol all --port -1 --source-group $sgasg
